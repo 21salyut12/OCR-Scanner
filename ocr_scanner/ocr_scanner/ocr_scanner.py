271462792a6d4b52f36efa98c8af8a986805ae6a
Ciocoiu Radu-Mihail <58552434+21salyut12@users.noreply.github.com>
2023-04-26 16:54:36 +0300

    Add files via upload
    Change code to open an image with OpenCV and not Tessarct, also added 2 new functionalities to the project: 1 to invert an image and 2 to binarize an image (grey scale the image/photo).

---
+++ ocr_scanner/ocr_scanner/ocr_scanner.py
@@ -0,0 +1,55 @@
+import cv2
+import numpy
+import pytesseract
+from PIL import Image
+from matplotlib import pyplot as plot
+
+#Open Saved Photo
+img_file = 'images/camera-test.jpg'
+
+image = cv2.imread(img_file)
+
+def display_img(img_path):
+    dpi = 80
+    img_data = plot.imread(img_path)
+    height, width = img_data.shape[:2]
+
+    figsize = width / float(dpi), height / float(dpi)
+    fig = plot.figure(figsize=figsize)
+    ax = fig.add_axes([0, 0, 1, 1])
+    ax.axis('off')
+    ax.imshow(img_data, cmap='gray')
+    plot.show()
+
+display_img(img_file)
+
+#Invert Image/Photo
+inverted_img = cv2.bitwise_not(image)
+cv2.imwrite('processed_images/inverted_camera-test.jpg', inverted_img)
+display_img('processed_images/inverted_camera-test.jpg')
+
+#Binarization
+def grayscale (img):
+    return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+gray_image = grayscale(image)
+cv2.imwrite('processed_images/gray.jpg', gray_image)
+
+display_img('processed_images/gray.jpg')
+
+
+"""
+img_files_list = ["images/camera-test.jpg", "images/test_image1.jpg",
+                  "images/test_image2.jpg", "images/test_image3.png"]
+
+
+try:
+    for img_file in img_files_list:
+        img = Image.open(img_file)
+        ocr = pytesseract.image_to_string(img)
+        print('Scanned result for {}: \n{}'.format(img_file, ocr))
+
+#    img.save("processed_images/proc_camera-test.jpg")
+except IOError:
+    pass
+"""
