6fa9980099d24ceccb0fc9950425eae7fa45e107
Ciocoiu Radu-Mihail <58552434+21salyut12@users.noreply.github.com>
2023-04-29 12:48:19 +0300

    Border functions
    Added function to remove borders and function to add missing borders

--- ocr_scanner/ocr_scanner.py
+++ ocr_scanner/ocr_scanner.py
@@ -9,6 +9,8 @@ img_file = 'images/capture-test.jpg'
 
 image = cv2.imread(img_file)
 
+
+#Display image
 def display_img(img_path):
     dpi = 80
     img_data = plot.imread(img_path)
@@ -84,11 +86,34 @@ dilated_image = thick_font(no_noise)
 cv2.imwrite('processed_images/dilated_image.jpg', dilated_image)
 
 display_img('processed_images/dilated_image.jpg')
+
+#Removing borders
+def remove_borders(image):
+    contours, heiarchy = cv2.findContours(image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cntsSorted = sorted(contours, key=lambda x:cv2.contourArea(x))
+    cnt = cntsSorted[-1]
+    x, y, w, h = cv2.boundingRect(cnt)
+    crop = image[y:y+h, x:x+w]
+    return (crop)
+
+no_borders = remove_borders(no_noise)
+cv2.imwrite('processed_images/removed_borders.jpg', no_borders)
+
+display_img('processed_images/removed_borders.jpg')
+
+#Adding missing borders
+color = [255, 255, 255]
+top, bottom, left, right = [150]*4
+
+image_with_borders = cv2.copyMakeBorder(no_borders, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)
+cv2.imwrite("processed_images/image_with_borders.jpg", image_with_borders)
+
+display_img("processed_images/image_with_borders.jpg")
+
 try:
         img = Image.open(img_file)
         ocr = pytesseract.image_to_string(img)
         print('Scanned result for {}: \n{}'.format(img_file, ocr))
-
 #    img.save("processed_images/proc_camera-test.jpg")
 except IOError:
     pass
